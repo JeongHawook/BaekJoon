79a02c8ec04c477d2a7b2f18126d6b193052d12b
LJ <126029736+JeongHawook@users.noreply.github.com>
2023-04-07 14:40:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = '';
+
+
+        for(i=0 ;i <n; i++){
+        if(i%2!=0){
+          answer += "박"  
+        } else{
+            answer+="수"
+        }
+            
+
+   
+}
+     return answer;
+}
