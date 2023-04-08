09d17205229b8e3c67b01cac85a6c10f93e6e4ad
LJ <126029736+JeongHawook@users.noreply.github.com>
2023-04-08 12:03:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,18 +1,8 @@
 function solution(s) {
-    var answer = true;
 
-   if(s.length == 4|| s.length == 6) {
-       
-    if(parseInt(s)==s){
-       return true;
-   
-    }else{
-       return false
-   
-    }
-    
-   
-   }
-    return false
-    
+    var regex = /^\d{6}$|^\d{4}$/;
+
+  return regex.test(s);
 }
+    
+
