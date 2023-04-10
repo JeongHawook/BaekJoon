74404774cefd262db28df7a244a3f4f69601f053
LJ <126029736+JeongHawook@users.noreply.github.com>
2023-04-11 04:05:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,28 @@
+function solution(n, m) {
+    var answer = [];
+    
+    
+    
+    let result = rotate(m,n)
+    console.log(result)
+    
+    answer[0]= result
+    answer[1]= (n*m)/result
+    
+    return answer;
+}
+
+function rotate(large , small){
+    
+    let temp = large%small
+    
+    if(temp == 0){
+        return small;
+    }
+    
+    
+    return rotate(small, temp)
+    
+    
+    
+}
