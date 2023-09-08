8a13706ecce51afaf1003680d7df82210c884bdc
LJ <126029736+jeonghawook@users.noreply.github.com>
2023-09-08 13:01:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,25 +1,18 @@
 function solution(s) {
-    let numbers = s.split(" ").map(Number); // Convert the split strings to numbers
-    let max = -Infinity; // Initialize max with negative infinity
-    let min = Infinity; // Initialize min with positive infinity
-
-    for (let i = 0; i < numbers.length; i++) {
-        if (min > numbers[i]) {
-            min = numbers[i];
+    var answer = '';
+    let numbers = (s.split(" "))
+    let max = 0;
+    for(i=0;i<numbers.length;i++){
+        for(k=i+1;k<numbers.length;k++){
+            
+            if(numbers[i]<numbers[k]){
+                max = numbers[i]
+                 
+            }
         }
-        if (max < numbers[i]) {
-            max = numbers[i];
-        }
+      
+        
     }
-
-    let final = min + " " + max; // Create the final string in the required format
-    return final;
+   console.log(max)
+    return answer;
 }
-
-// Example usage:
-const input1 = "1 2 3 4";
-const input2 = "-1 -2 -3 -4";
-const input3 = "-1 -1";
-console.log(solution(input1)); // Output: "1 4"
-console.log(solution(input2)); // Output: "-4 -1"
-console.log(solution(input3)); // Output: "-1 -1"
